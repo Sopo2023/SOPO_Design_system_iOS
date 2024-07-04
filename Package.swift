da9a77f875f2d7dcddbea8bfe145a57d3391c84d
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-07-05 02:40:45 +0900

    fix: 디렉토리 옮김

--- Package.swift
+++ Package.swift
@@ -14,7 +14,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SDS")
+        .target(name: "SDS",
+                resources: [
+                    .process("Resources/Color")
+                ]
+               )
     ]
 )
