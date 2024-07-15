f70773622e7ae32d7082ffee93db56c1466242ea
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-07-15 14:06:09 +0900

    fix: static

--- Sources/SDS/Foundation/Color/Color+Ext.swift
+++ Sources/SDS/Foundation/Color/Color+Ext.swift
@@ -2,15 +2,14 @@ import SwiftUI
 
 extension SopoColor {
     protocol CanColor {
-        var w100: Color { get set }
-        var w200: Color { get set }
-        var w300: Color { get set }
-        var w400: Color { get set }
-        var w500: Color { get set }
-        var w600: Color { get set }
-        var w700: Color { get set }
-        var w800: Color { get set }
-        var w900: Color { get set }
-
+        static var w100: Color { get set }
+        static var w200: Color { get set }
+        static var w300: Color { get set }
+        static var w400: Color { get set }
+        static var w500: Color { get set }
+        static var w600: Color { get set }
+        static var w700: Color { get set }
+        static var w800: Color { get set }
+        static var w900: Color { get set }
     }
 }
--- Sources/SDS/Foundation/Color/Pallete+Ext.swift
+++ Sources/SDS/Foundation/Color/Pallete+Ext.swift
@@ -11,16 +11,15 @@ extension SopoColor.Pallete.Common {
 
 extension SopoColor.Pallete.Primary {
     public struct Green: SopoColor.CanColor {
-        var w100: Color = Color("Green100")
-        var w200: Color = Color("Green200")
-        var w300: Color = Color("Green300")
-        var w400: Color = Color("Green400")
-        var w500: Color = Color("Green500")
-        var w600: Color = Color("Green600")
-        var w700: Color = Color("Green700")
-        var w800: Color = Color("Green800")
-        var w900: Color = Color("Green900")
-        
+        static var w100: Color = Color("Green100")
+        static var w200: Color = Color("Green200")
+        static var w300: Color = Color("Green300")
+        static var w400: Color = Color("Green400")
+        static var w500: Color = Color("Green500")
+        static var w600: Color = Color("Green600")
+        static var w700: Color = Color("Green700")
+        static var w800: Color = Color("Green800")
+        static var w900: Color = Color("Green900")
     }
     
     public struct LightGray { }
