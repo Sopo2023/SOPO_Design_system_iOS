e31bc4b3af01d5d859c4c28f356f01745c5065c6
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-07-16 14:11:35 +0900

    feat: Color add

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "SDS",
                 resources: [
-                    .process("Resources/Color")
+                    .copy("Resources/Color")
                 ]
                )
     ]
--- Sources/SDS/Foundation/Color/Pallete+Ext.swift
+++ Sources/SDS/Foundation/Color/Pallete+Ext.swift
@@ -10,9 +10,10 @@ extension SopoColor.Pallete.Common {
 }
 
 extension SopoColor.Pallete.Primary {
+    
     public enum Green: SopoColor.CanColor {
         case w100, w200, w300, w400, w500, w600, w700, w800, w900
-        
+
         public var color: Color {
             switch self {
             case .w100:
@@ -35,9 +36,60 @@ extension SopoColor.Pallete.Primary {
                 return Color("Green900")
             }
         }
+    }
+    
+    public enum LightGray: SopoColor.CanColor {
+        case w100, w200, w300, w400, w500, w600, w700, w800, w900
         
+        public var color: Color {
+            switch self {
+            case .w100:
+                return Color("LightGray100")
+            case .w200:
+                return Color("LightGray200")
+            case .w300:
+                return Color("LightGray300")
+            case .w400:
+                return Color("LightGray400")
+            case .w500:
+                return Color("LightGray500")
+            case .w600:
+                return Color("LightGray600")
+            case .w700:
+                return Color("LightGray700")
+            case .w800:
+                return Color("LightGray800")
+            case .w900:
+                return Color("LightGray900")
+            }
+        }
+    }
+    
+    public enum DarkGray: SopoColor.CanColor {
+        case w100, w200, w300, w400, w500, w600, w700, w800, w900
         
-        
+        public var color: Color {
+            switch self {
+            case .w100:
+                return Color("DarkGray100")
+            case .w200:
+                return Color("DarkGray200")
+            case .w300:
+                return Color("DarkGray300")
+            case .w400:
+                return Color("DarkGray400")
+            case .w500:
+                return Color("DarkGray500")
+            case .w600:
+                return Color("DarkGray600")
+            case .w700:
+                return Color("DarkGray700")
+            case .w800:
+                return Color("DarkGray800")
+            case .w900:
+                return Color("DarkGray900")
+            }
+        }
     }
 }
 
