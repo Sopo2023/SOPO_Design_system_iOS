feda0b255cfaeb28177f03a3824c8c47fbff97be
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-07-22 16:35:43 +0900

    fix: Resources 경로

--- Sources/SDS/Foundation/Color/Color+Ext.swift
+++ Sources/SDS/Foundation/Color/Color+Ext.swift
@@ -1,5 +1,8 @@
 import SwiftUI
 
 extension Color {
-    
+    static let sementic = SopoColor.Sementic.self
 }
+
+
+
--- Sources/SDS/Foundation/Color/Pallete+Ext.swift
+++ Sources/SDS/Foundation/Color/Pallete+Ext.swift
@@ -2,104 +2,23 @@ import SwiftUI
 
 extension SopoColor.Pallete {
     public enum Common { case w0, w100 }
-    public enum Primary { }
+    public enum Neutral { case w5, w10, w15, w20, w25, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum Green { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum Gray { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum DarkGray { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum Yellow { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum Blue { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
+    public enum Red { case w10, w20, w30, w40, w50, w60, w70, w80, w90, w95, w99 }
 }
 
 extension SopoColor.Pallete.Common: SopoColor.CanColor {
     public var color: Color {
         switch self {
         case .w0:
-            return Color("Common0", bundle: .module)
+            return Color("Common/w0", bundle: .module)
         case .w100:
-            return Color("Common100", bundle: .module)
-        }
-    }
-    
-    
-}
-
-extension SopoColor.Pallete.Primary {
-    
-    public enum Green: SopoColor.CanColor {
-        case w100, w200, w300, w400, w500, w600, w700, w800, w900
-
-        public var color: Color {
-            switch self {
-            case .w100:
-                return Color("Green200", bundle: .module)
-            case .w200:
-                return Color("Green200", bundle: .module)
-            case .w300:
-                return Color("Green300", bundle: .module)
-            case .w400:
-                return Color("Green400", bundle: .module)
-            case .w500:
-                return Color("Green500", bundle: .module)
-            case .w600:
-                return Color("Green600", bundle: .module)
-            case .w700:
-                return Color("Green700", bundle: .module)
-            case .w800:
-                return Color("Green800", bundle: .module)
-            case .w900:
-                return Color("Green900", bundle: .module)
-            }
-        }
-    }
-    
-    public enum LightGray: SopoColor.CanColor {
-        case w100, w200, w300, w400, w500, w600, w700, w800, w900
-        
-        public var color: Color {
-            switch self {
-            case .w100:
-                return Color("LightGray100", bundle: .module)
-            case .w200:
-                return Color("LightGray200", bundle: .module)
-            case .w300:
-                return Color("LightGray300", bundle: .module)
-            case .w400:
-                return Color("LightGray400", bundle: .module)
-            case .w500:
-                return Color("LightGray500", bundle: .module)
-            case .w600:
-                return Color("LightGray600", bundle: .module)
-            case .w700:
-                return Color("LightGray700", bundle: .module)
-            case .w800:
-                return Color("LightGray800", bundle: .module)
-            case .w900:
-                return Color("LightGray900", bundle: .module)
-            }
-        }
-    }
-    
-    public enum DarkGray: SopoColor.CanColor {
-        case w100, w200, w300, w400, w500, w600, w700, w800, w900
-        
-        public var color: Color {
-            switch self {
-            case .w100:
-                return Color("DarkGray100", bundle: .module)
-            case .w200:
-                return Color("DarkGray200", bundle: .module)
-            case .w300:
-                return Color("DarkGray300", bundle: .module)
-            case .w400:
-                return Color("DarkGray400", bundle: .module)
-            case .w500:
-                return Color("DarkGray500", bundle: .module)
-            case .w600:
-                return Color("DarkGray600", bundle: .module)
-            case .w700:
-                return Color("DarkGray700", bundle: .module)
-            case .w800:
-                return Color("DarkGray800", bundle: .module)
-            case .w900:
-                return Color("DarkGray900", bundle: .module)
-            }
+            return Color("Common/w100", bundle: .module)
         }
     }
 }
-
 
