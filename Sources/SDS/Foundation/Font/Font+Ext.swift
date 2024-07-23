809f719a644a84fe8cc6796e84a9a6c0a9f179bf
신민호 <68156836+alsh0807@users.noreply.github.com>
2024-07-23 14:16:21 +0900

    fix: static issue

--- Sources/SDS/Foundation/Font/Font+Ext.swift
+++ Sources/SDS/Foundation/Font/Font+Ext.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 extension Font {
-    public func pretendard( _ weight: SopoFont.Pretendard = .regular, size: CGFloat ) -> Font {
+    public static func pretendard( _ weight: SopoFont.Pretendard = .regular, size: CGFloat ) -> Font {
         return weight.font(size: size)
     }
 }
